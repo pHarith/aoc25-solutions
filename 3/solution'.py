cda3db079ca2b8567d1b2d0ce4bfa5480a50ea74
suonseap <seappharith.suon@mail.utoronto.ca>
2026-05-30 15:45:01 -0400

    initialized working directory for aoc 2025 solutions

---
+++ 3/solution'.py
@@ -0,0 +1,31 @@
+# Solution to Advent of Code 20XX
+# Day X : 
+
+#### SUMMARY OF TASKS ####
+# 1. 
+
+
+#### Helper Functions Goes Here (if any) ####
+
+
+def solve(input_file):
+    """
+    Produce the solution to day x problem - 
+    """
+    return
+
+#### Helper Functions For Part 2 Goes Here (if any) ####
+
+
+#### Part 2 Goes Here ####
+def solve_part2(input_file):
+    """
+    Produce the solution to part 2 of day x problem - 
+    """
+    return
+
+
+if __name__ == "__main__":
+    input = 'input.txt'
+    # input = 'test.txt'
+    print(f"The solution is {solve(input)}.")
