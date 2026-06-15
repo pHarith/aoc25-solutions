c28dd493fafe2e2311e406afe71721774a85a82b
suonseap <seappharith.suon@mail.utoronto.ca>
2026-06-15 01:25:52 -0400

    solved part of day 6 aco 2025, adding comments and docstrings later

--- 6/solution.py
+++ 6/solution.py
@@ -2,17 +2,45 @@
 # Day 6: Trash Compactor
 
 #### SUMMARY OF TASKS ####
-# 1. 
+# 1. Read the math problems from file into a nested list of numbers involved and the list of operations
+# 2. For each column, grab the all the numbers from the same column and the corresponding operation 
+# to compute
+# 3. Store the results into a list. Return the sum of said list.
+
+from math import prod
 
 
 #### Helper Functions Goes Here (if any) ####
+def read_math_problem(input_file):
+    with open(input_file, "r") as f:
+        math_problem = [s.strip().split() for s in f.readlines()]
+    
+    # Operations are always the last row in the file
+    return [[int(num) for num in num_lst] for num_lst in math_problem[:-1]], math_problem[-1]
 
+
+def perform_operation(lst, opr):
+    match opr:
+        case '+':
+            return sum(lst)
+        case '*':
+            return prod(lst)
 
 def solve(input_file):
     """
     Produce the solution to Day 6: Trash Compactor
     """
-    return
+    results = []
+
+    nums, ops = read_math_problem(input_file)
+    
+    column_nums = list(zip(*nums))
+    print(column_nums)
+
+    for i in range(len(ops)):
+        results.append(perform_operation(column_nums[i], ops[i]))
+    
+    return sum(results)
 
 #### Helper Functions For Part 2 Goes Here (if any) ####
 
