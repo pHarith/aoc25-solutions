57654db59a2fb74f077793d0912b5b0de56ede4c
suonseap <seappharith.suon@mail.utoronto.ca>
2026-06-20 21:55:54 -0400

    added test files, todos, notes, code skeletons and comments for day 7

--- 7/solution.py
+++ 7/solution.py
@@ -2,16 +2,38 @@
 # Day 7: Laboratories
 
 #### SUMMARY OF TASKS ####
-# 1. 
+# 1. Read the teleportation board
+# 2. Grabbing the start coordinates 
 
+
+# Global Variables
+START = "S"
+SPLITTER = "^"
 
 #### Helper Functions Goes Here (if any) ####
+
+def read_board(input_file):
+    """
+    Read a board of m row x n cols into a matrix.
+    """
+    board = []
+
+    # TODO: read files
+
+    return board
+
+def find_start(board):
+    start_row, start_col = 0, 0
+    
+    # TODO: Return the row, col of the start object (S)
+    return (start_row, start_col)
 
 
 def solve(input_file):
     """
     Produce the solution to Day 7: Laboratories 
     """
+    # TODO: return the number of times the tachyon beam is split
     return
 
 #### Helper Functions For Part 2 Goes Here (if any) ####
