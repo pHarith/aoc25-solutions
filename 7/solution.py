39f82e48d2a05e2d5f37b610ef3dd9c75e4d6cca
suonseap <seappharith.suon@mail.utoronto.ca>
2026-06-22 22:58:56 -0400

    solved part 1, day 7 aoc 2025

--- 7/solution.py
+++ 7/solution.py
@@ -16,25 +16,68 @@ def read_board(input_file):
     """
     Read a board of m row x n cols into a matrix.
     """
-    board = []
-
-    # TODO: read files
-
-    return board
+    with open(input_file, "r") as file:
+        return [[space for space in row.strip()] for row in file]
 
 def find_start(board):
-    start_row, start_col = 0, 0
+    num_rows, num_cols = len(board), len(board[0])
     
     # TODO: Return the row, col of the start object (S)
-    return (start_row, start_col)
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if board[row][col] == START:
+                return (row, col)
+
+    return (-1, -1)
+
+def split_count(board):
+    num_rows, num_cols = len(board), len(board[0])
+    split_count = 0
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if board[row][col] == SPLITTER:
+                split_count += 1
+
+    return split_count
+
 
 
 def solve(input_file):
     """
     Produce the solution to Day 7: Laboratories 
     """
+    def is_in_bounds(cell):
+        return (0 <= cell[0] < num_rows) and (0 <= cell[1] < num_cols)
+
+
     # TODO: return the number of times the tachyon beam is split
-    return
+    board = read_board(input)
+    start = find_start(board)
+
+    num_rows, num_cols = len(board), len(board[0])
+    to_visit = [start]
+    split_occurences = []
+
+    while to_visit:
+        curr = to_visit.pop()
+
+        next = (curr[0] + 1, curr[1])
+
+        if is_in_bounds(next):
+            to_visit.append(next)
+            
+            if board[next[0]][next[1]] == SPLITTER:
+                
+                to_visit.pop()
+
+                split = (curr, next)
+
+                if split not in split_occurences:
+                    split_occurences.append(split)
+                    to_visit.extend([(next[0], next[1] + 1), (next[0], next[1] - 1)])
+                    
+    return len(split_occurences)
 
 #### Helper Functions For Part 2 Goes Here (if any) ####
 
